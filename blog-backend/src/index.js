51c29ad461563d598ac7f03ebd65f283111b9539
SOO <aubepluieh3@naver.com>
2022-07-06 16:22:48 +0900

    mongoDB and mongoose

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,9 +1,22 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
-
+const mongoose = require('mongoose');
 const api = require('./api');
 
+const {
+    PORT: port =4000,
+    MONGO_URI:mongoURL
+} = process.env;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURL).then(()=>{
+    console.log('connected to mongodb');
+}).catch((e) => {
+        console.error(e);
+});
 const app = new Koa();
 const router = new Router();
 
@@ -14,6 +27,6 @@ app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
 
-app.listen(4000, () => {
-    console.log('listening to port 4000');
+app.listen(port, () => {
+    console.log('listening to port',port);
 });
