ac79b3cbf81c45684216bca0231e44ac8f278f90
SOO <aubepluieh3@naver.com>
2022-07-13 13:15:36 +0900

    ModalWrapper

--- blog-frontend/src/components/modal/LoginModal/LoginModal.js
+++ blog-frontend/src/components/modal/LoginModal/LoginModal.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './LoginModal.scss';
 import classNames from 'classnames/bind';
+import ModalWrapper from '../ModalWrapper';
 
 const cx = classNames.bind(styles);
 
 const LoginModal = ({
     visible, password, error, onCancel, onLogin, onChange, onKeyPress
 })=>(
-    <div visible={visible}>
+    <ModalWrapper visible={visible}>
         <div className={cx('form')}>
             <div onClick={onCancel} className={cx('close')}>&times;</div>
             <div className={cx('title')}>로그인</div>
@@ -16,7 +17,7 @@ const LoginModal = ({
             {error && <div className={cx('error')}>로그인 실패 </div>}
             <div className = {cx('login')} onClick={onLogin}>로그인</div>
         </div>
-    </div>
+    </ModalWrapper>
 );
 
 export default LoginModal;
